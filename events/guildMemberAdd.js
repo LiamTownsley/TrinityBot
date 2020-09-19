645410d5b9cc0a7378ff6dbf3361ea09bd093177
Liam Townsley <liamtownsley42@gmail.com>
2020-09-19 18:58:28 +0100

    feat: autorole

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -6,4 +6,6 @@ module.exports = async (client, member) => {
 
 	const goal = await client.channels.fetch('756916893507518604');
 	await goal.setName(`Goal: ${Math.ceil(member.guild.memberCount / 100) * 100}`);
+
+	member.roles.add('756864400836526100', 'Autorole');
 };
